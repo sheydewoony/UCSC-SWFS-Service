ca31074219e3265f1737fc6b0ab376e43a04ee5a
Rishmi Shehan <shehansc2010@gmail.com>
2023-05-13 13:30:06 +0530

    Add git ignore

---
+++ UCSC.SWFS.SRV.Entity/Entities/Audit/AuditFields.cs
@@ -0,0 +1,11 @@
+﻿namespace UCSC.SWFS.SRV.Entity.Entities.Audit
+{
+    public class AuditFields
+    {
+        public bool IsActive { get; set; } = true;
+        public int CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
---
+++ UCSC.SWFS.SRV.Entity/Entities/UserManagemen/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCSC.SWFS.SRV.Entity.Entities.Audit;
+
+namespace UCSC.SWFS.SRV.Entity.Entities.UserManagemen
+{
+    public class User : AuditFields
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
